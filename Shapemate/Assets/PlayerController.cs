b92a4118cee609f4b1728cdb17eafa397a8d2be9
dbakti7 <dbakti1605@gmail.com>
2016-11-11 20:28:25 +0800

    init

---
+++ Shapemate/Assets/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+    public Rigidbody2D objectRb;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+    void FixedUpdate()
+    {
+
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            objectRb.AddForce(new Vector2(1, 1));
+        }
+    }
+}
