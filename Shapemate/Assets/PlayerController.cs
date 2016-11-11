bda073a8f76491c2028888eca92f52938c5d2a10
dbakti7 <dbakti1605@gmail.com>
2016-11-11 22:34:51 +0800

    added movement

--- Shapemate/Assets/PlayerController.cs
+++ Shapemate/Assets/PlayerController.cs
@@ -1,25 +1,59 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
     public Rigidbody2D objectRb;
+    public bool movingRight = true;
+    public bool grounded = true;
+    public float speed = 10.0f;
+    public float jumpSpeed = 20.0f;
+    public float gravity = 35.0f;
+    public float yPosition = 0.0f;
+    public bool falling = false;
+    private Vector3 moveDirection = Vector3.zero;
+    // Use this for initialization
+    void Start()
+    {
+        yPosition = objectRb.transform.position.y;
+    }
 
-	// Use this for initialization
-	void Start () {
-	    
-	}
-	
     void FixedUpdate()
     {
 
     }
 
-	// Update is called once per frame
-	void Update ()
+    // Update is called once per frame
+    void Update()
     {
+
+        if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D))
+            objectRb.velocity = new Vector2(0, objectRb.velocity.y);
+        if (Input.GetKey(KeyCode.A))
+        {
+            if (movingRight)
+                movingRight = false;
+            objectRb.velocity = new Vector2(-speed, objectRb.velocity.y);
+            //objectRb.AddForce(new Vector2(-5, 0));
+        }
         if (Input.GetKey(KeyCode.D))
         {
-            objectRb.AddForce(new Vector2(1, 1));
+            if (!movingRight)
+                movingRight = true;
+            objectRb.velocity = new Vector2(speed, objectRb.velocity.y);
+            //objectRb.AddForce(new Vector2(5, 0));
         }
+        if (Input.GetKey(KeyCode.W) && grounded)
+        {
+            objectRb.velocity = new Vector2(objectRb.velocity.x, jumpSpeed);
+            grounded = false;
+        }
+        objectRb.velocity = new Vector2(objectRb.velocity.x, objectRb.velocity.y - gravity * Time.deltaTime);
+
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Ground")
+            grounded = true;
     }
 }
