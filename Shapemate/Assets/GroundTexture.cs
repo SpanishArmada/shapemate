49ab430dae7ce1363477b2890ff76340e6553c6e
kenrick95 <kenrick95@gmail.com>
2016-11-11 22:12:07 +0800

    add programmatically texture

---
+++ Shapemate/Assets/GroundTexture.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class GroundTexture : MonoBehaviour {
+    public Material baseMaterial;
+    private Material groundMaterial;
+
+    // Use this for initialization
+    void Start ()
+    {
+        UpdateTextureScale();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+    }
+
+    void UpdateTextureScale()
+    {
+        Renderer renderer = GetComponent<Renderer>();
+        double width = renderer.bounds.size.x;
+        double height = renderer.bounds.size.y;
+
+
+        Material groundMaterial = new Material(baseMaterial);
+        groundMaterial.mainTextureScale = new Vector2((float)width, (float)height);
+        //groundMaterial.mainTextureOffset = new Vector2((float)width % 32, (float)height % 32);
+
+        renderer.material = groundMaterial;
+    }
+
+    
+}
