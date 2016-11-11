849c9e34014172ac9a8090caaec92cce19bad0a9
VotBear <vot1_bear@yahoo.com>
2016-11-12 01:04:54 +0800

    Aristo

---
+++ Shapemate/Assets/_Scripts/Friend/Friend.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Friend : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	protected virtual void reachedTarget(){
+	}
+}
