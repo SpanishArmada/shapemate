26da882f020ff6fee1d598fcbf9d3f9cb8ea31f7
dbakti7 <dbakti1605@gmail.com>
2016-11-12 02:25:43 +0800

    parallax

---
+++ Shapemate/Assets/ParralaxScroll.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParralaxScroll : MonoBehaviour {
+    public Transform playerPos;
+    public float parallaxFactor;
+    private float offset;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector2((playerPos.position.x*parallaxFactor), playerPos.position.y);
+        if (GetComponent<Renderer>().material.mainTextureOffset.x > 1.0f)
+            GetComponent<Renderer>().material.mainTextureOffset = new Vector2(GetComponent<Renderer>().material.mainTextureOffset.x - 1.0f, GetComponent<Renderer>().material.mainTextureOffset.y);
+
+    }
+}
