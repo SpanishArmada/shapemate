353c1fdb7f9eead861a26e3385169b5e19f3f791
cacad101 <gbbpentium@gmail.com>
2016-11-11 23:30:38 +0800

    enemy HP manager

---
+++ Shapemate/Assets/EnemyHPManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyHPManager : MonoBehaviour {
+
+    public GameObject enemy;
+
+    private int enemyHP;
+
+	// Use this for initialization
+	void DealDamage (int damage=1) {
+        enemyHP -= damage;
+        if(enemyHP < 0)
+        {
+            Destroy(enemy);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
