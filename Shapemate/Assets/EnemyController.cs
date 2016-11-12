cfbd81779bd089253655dc2bf406ea818a93cdcb
cacad101 <gbbpentium@gmail.com>
2016-11-12 09:38:54 +0800

    enemy death

---
+++ Shapemate/Assets/EnemyController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+
+    public EnemyMovement thisMovement;
+    public EnemyUpDown thisUpDown;
+    public EnemyHPManager thisHP;
+    public Rigidbody2D thisRB;
+
+    private bool isDeath;
+
+    // Use this for initialization
+    void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (isDeath)
+        {
+            gameObject.layer = 9;
+
+            if(thisMovement!=null)
+                thisMovement.speed = 0;
+
+            if(thisRB!=null)
+                thisRB.velocity = new Vector2(thisRB.velocity.x, thisRB.velocity.y - 35 * Time.deltaTime);
+        }
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        //Change to Triangle Tag
+        if (col.gameObject.tag == "Player")
+        {
+            // reduce hp here
+            if (thisHP != null && thisHP.DealDamage(10f))
+            {
+                isDeath = true;
+            }
+        }
+    }
+}
--- Shapemate/Assets/_Scripts/PlayerController.cs
+++ Shapemate/Assets/_Scripts/PlayerController.cs
@@ -31,6 +31,7 @@ public class PlayerController : MonoBehaviour
         {
             objectRb.transform.position = latestCheckPoint;
             objectRb.velocity = new Vector2(0, -20);
+            selfHP.ResetHealth();
             isDeath = false;
         }
     }
@@ -74,9 +75,16 @@ public class PlayerController : MonoBehaviour
     {
         if (col.gameObject.tag == "Ground")
             grounded = true;
+        //if (col.gameObject.tag == "Enemy")
+        //    isDeath = true;
         if (col.gameObject.tag == "Enemy")
-            isDeath = true;
-        
+        {
+            // reduce hp here
+            if (selfHP.DealDamage(10f))
+            {
+                isDeath = true;
+            }
+        }
     }
     void OnTriggerEnter2D(Collider2D col)
     {
@@ -95,5 +103,6 @@ public class PlayerController : MonoBehaviour
                 isDeath = true;
             }
         }
+        
     }
 }
