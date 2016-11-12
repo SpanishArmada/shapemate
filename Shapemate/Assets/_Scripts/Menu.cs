c63ba7a79652173766fdb649170fe4a8b623476a
dbakti7 <dbakti1605@gmail.com>
2016-11-12 08:57:12 +0800

    Added Menu

---
+++ Shapemate/Assets/_Scripts/Menu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+    public GameObject loadingImage;
+    public void loadStage(int stage)
+    {
+        loadingImage.SetActive(true);
+        Application.LoadLevel(stage);
+    }
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
