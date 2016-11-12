e34548effe38cd193f6561889803cc5661aaef42
dbakti7 <dbakti1605@gmail.com>
2016-11-12 16:53:55 +0800

    fixed music

--- Shapemate/Assets/_Scripts/ChangeMusic.cs
+++ Shapemate/Assets/_Scripts/ChangeMusic.cs
@@ -3,8 +3,8 @@ using System.Collections;
 
 public class ChangeMusic : MonoBehaviour {
 
+    public AudioClip level1Music;
     public AudioClip level2Music;
-
     private AudioSource source;
 
     void Awake()
@@ -14,7 +14,12 @@ public class ChangeMusic : MonoBehaviour {
 
     void OnLevelWasLoaded(int level)
     {
-        if(level == 2)
+        if (level == 1)
+        {
+            source.clip = level1Music;
+            source.Play();
+        }
+        if (level == 2)
         {
             source.clip = level2Music;
             source.Play();
